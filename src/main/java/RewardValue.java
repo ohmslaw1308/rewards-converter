8970ad4a72b0ba327d88fbe69030f4619218603d
Ohm Bhakta <obhakta179@student.fuhsd.org>
2024-01-13 15:19:04 -0800

    implimented converter for miles and cash

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,24 @@
+public class RewardValue {
+
+    private int miles;
+    private double cash;
+
+    public RewardValue(double cash){
+        this.cash = cash;
+        this.miles = 0;
+    }
+
+    public RewardValue(int miles){
+        this.cash = 0;
+        this.miles = miles;
+    }
+
+    public double getCashValue(){
+        return miles * 0.0035;
+    }
+
+    public double getMilesValue(){
+
+        return cash / 0.0035;
+    }
+}
